7926304abd56eb49e517f255cb01d259a8a1f748
Roman Krysyaev <roman-krysyaev@yandex.ru>
2022-08-07 18:10:20 +0300

    Добавьте файлы проекта.

---
+++ RadioPlugin/RadioControl.xaml.cs
@@ -0,0 +1,92 @@
+﻿using Ascon.AU.PluginConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RadioPlugin
+{
+    /// <summary>
+    /// Логика взаимодействия для RadioControl.xaml
+    /// </summary>
+    public partial class RadioControl : UserControl, IPlugin
+    {
+        public string DisplayName { get; set; } = "Радио";
+        public string Description { get; set; } = "Радио";
+        public string PluginID { get; set; } = "RadioPlugin";
+        public int DisplayIconCode { get; set; } = 0xe009;
+        public string HelpLink { get; set; } = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+        public string UpdateCommandDescription { get; set; }
+        public string ExpressPanelCommandDescription { get; set; }
+        public string Command3Description { get; set; }
+        public string Command4Description { get; set; }
+        public string Command5Description { get; set; }
+
+        public event EventHandler<ToastNotificationEventArgs> OnShowToastNotification;
+
+        public UserControl GetPlugin() => this;
+
+        public RadioViewModel ViewModel;
+
+        public void Initialize()
+        {
+            ViewModel = new RadioViewModel();
+            this.DataContext = ViewModel;
+        }
+
+        public void Terminate()
+        {
+            
+        }
+
+        public void UpdateCommand()
+        {
+            
+        }
+
+        public void ExpressPanelCommand()
+        {
+            
+        }
+
+        public void Command3()
+        {
+            
+        }
+
+        public void Command4()
+        {
+            
+        }
+
+        public void Command5()
+        {
+            
+        }
+
+        public RadioControl()
+        {
+            InitializeComponent();
+        }
+
+        private void strt_btn_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.Start();
+        }
+
+        private void stp_btn_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.Stop();
+        }
+    }
+}
---
+++ RadioPlugin/RadioViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WMPLib;
+
+namespace RadioPlugin
+{
+    public class RadioViewModel : INotifyPropertyChanged
+    {
+        //Property changed
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private WindowsMediaPlayer wmp;
+        public WindowsMediaPlayer WMP
+        {
+            get => wmp;
+            set
+            {
+                wmp = value;
+                OnPropertyChanged(nameof(WMP));
+            }
+        }
+
+        private int volume = 100;
+        public int Volume
+        {
+            get => volume;
+            set
+            {
+                volume = value;
+                OnPropertyChanged(nameof(Volume));
+            }
+        }
+
+        private string url = "http://213.59.4.27:8000/silver128.mp3";
+        public string URL
+        {
+            get => url;
+            set
+            {
+                url = value;
+                OnPropertyChanged(nameof(URL));
+            }
+        }
+
+        public RadioViewModel()
+        {
+            WMP = new WindowsMediaPlayer();
+        }
+
+        public void Start()
+        {
+            WMP.URL = URL;
+            WMP.settings.volume = Volume;
+            //WMP.controls.play();
+            Task play = new Task(WMP.controls.play);
+            play.Start();
+        }
+
+        public void Stop()
+        {
+            if (wmp.playState == WMPPlayState.wmppsPlaying)
+            {
+                WMP.controls.stop();
+            }
+        }
+    }
+}
